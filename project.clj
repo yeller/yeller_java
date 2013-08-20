bbef3a04bac4611e4b897686e1be5bd2b075b141
Tom Crayford <tcrayford@googlemail.com>
2013-08-20 11:31:03 +0100

    use lein for building/testing

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject yeller-java-api "0.0.1-SNAPSHOT"
+  :description "java client for yellerapp.com's exception tracking api"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "http://yellerapp.com/doc/java"
+  :java-source-paths ["src"]
+  :javac-options ["-target" "1.7" "-source" "1.7"]
+  :dependencies [[org.apache.httpcomponents/httpclient "4.2.5"]
+                 [com.fasterxml.jackson.core/jackson-core "2.2.2"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.2.2"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.2.2"]]
+  :profiles {:test {:dependencies [[junit/junit "4.11"]
+                                  [org.jmock/jmock-junit4 "2.6.0"]
+                                   [org.clojure/clojure "1.5.1"]
+                                   [org.hamcrest/hamcrest-all "1.3"]]
+                   :plugins {org.clojars.tcrayford/lein-junit "1.1.5-SNAPSHOT"}
+                   :junit ["test"]
+                   :junit-formatter :xml
+                   :junit-options {:fork "off"}
+                   :java-source-paths ["src" "test"]}})
