60cae6774aa97785b0a74423b72c295a9be57fe5
Tom Crayford <tcrayford@googlemail.com>
2013-08-22 10:53:47 +0100

    formatting

--- src/com/yellerapp/client/FormattedException.java
+++ src/com/yellerapp/client/FormattedException.java
@@ -10,10 +10,13 @@ public class FormattedException {
 
 	@JsonSerialize
 	public String type;
+
 	@JsonSerialize
 	public String message;
+
 	@JsonSerialize
 	public ArrayList<ArrayList<String>> stackTrace;
+
 	@JsonSerialize
 	public String host;
 
