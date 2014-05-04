f47fb9c23d070aa0be2daea9932d4551d4816c31
Tom Crayford <tcrayford@googlemail.com>
2014-05-04 13:47:49 +0100

    updated http client, fixed dep for junit

--- project.clj
+++ project.clj
@@ -4,13 +4,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "http://yellerapp.com/doc/java"
   :java-source-paths ["src"]
+  :resource-paths ["resources"]
   :javac-options ["-target" "1.7" "-source" "1.7"]
-  :dependencies [[org.apache.httpcomponents/httpclient "4.2.5"]
+  :dependencies [[org.apache.httpcomponents/httpclient "4.3"]
+                 [org.apache.httpcomponents/fluent-hc  "4.3"]
                  [com.fasterxml.jackson.core/jackson-core "2.2.2"]
                  [com.fasterxml.jackson.core/jackson-annotations "2.2.2"]
                  [com.fasterxml.jackson.core/jackson-databind "2.2.2"]]
-  :profiles {:test {:dependencies [[junit/junit "4.11"]
-                                  [org.jmock/jmock-junit4 "2.6.0"]
-                                   [org.clojure/clojure "1.5.1"]
-                                   [org.hamcrest/hamcrest-all "1.3"]]
+  :profiles {:test {:dependencies [[org.hamcrest/hamcrest-all "1.3"]
+                                   [junit/junit-dep "4.11"]
+                                   [org.jmock/jmock-junit4 "2.6.0"]
+                                   [org.clojure/clojure "1.5.1"]]
                     :java-source-paths ["src" "test"]}})
