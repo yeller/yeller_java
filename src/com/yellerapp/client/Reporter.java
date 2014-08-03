b2f16b9e080fd846965e742173fa66cea85d6ccc
Tom Crayford <tcrayford@googlemail.com>
2014-08-03 12:40:47 +0100

    ignore exceptions in development and testing environments

--- src/com/yellerapp/client/Reporter.java
+++ src/com/yellerapp/client/Reporter.java
@@ -8,7 +8,8 @@ public class Reporter {
 	private int currentBackend = 0;
 	private YellerErrorHandler handler;
 
-	public Reporter(String apiKey, String[] urls, HTTPClient http, YellerErrorHandler handler) {
+	public Reporter(String apiKey, String[] urls, HTTPClient http,
+			YellerErrorHandler handler) {
 		this.apiKey = apiKey;
 		this.urls = urls;
 		this.http = http;
@@ -16,7 +17,12 @@ public class Reporter {
 	}
 
 	public void report(FormattedException exception) {
-		report(exception, 0);
+		if (exception.applicationEnvironment.equals("test")
+				|| exception.applicationEnvironment.equals("development")) {
+			// ignore
+		} else {
+			report(exception, 0);
+		}
 	}
 
 	protected void report(FormattedException exception, int retryCount) {
@@ -26,18 +32,18 @@ public class Reporter {
 			try {
 				http.post(this.urls[this.currentBackend] + "/" + this.apiKey,
 						exception);
-                this.cycleBackend();
+				this.cycleBackend();
 			} catch (AuthorizationException e) {
 				this.handler.reportAuthError(this.urls[this.currentBackend], e);
 			} catch (Exception e) {
 				this.handler.reportIOError(this.urls[this.currentBackend], e);
-                this.cycleBackend();
+				this.cycleBackend();
 				report(exception, retryCount + 1);
 			}
 		}
 	}
 
-    protected synchronized void cycleBackend() {
-        this.currentBackend = (this.currentBackend + 1) % urls.length;
-    }
+	protected synchronized void cycleBackend() {
+		this.currentBackend = (this.currentBackend + 1) % urls.length;
+	}
 }
