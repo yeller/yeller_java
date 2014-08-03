e433fa4df85e6b5dabc57e6645e96cc15533709b
Tom Crayford <tcrayford@googlemail.com>
2014-08-03 12:41:57 +0100

    bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yeller-java-api "0.0.1-SNAPSHOT"
+(defproject yeller-java-api "0.0.2-SNAPSHOT"
   :description "java client for yellerapp.com's exception tracking api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
