b1a0fa21ee805f4c0d181271e70939b9e096ad84
Tom Crayford <tcrayford@googlemail.com>
2015-01-21 13:59:25 +0000

    introduce connection pooling into http client

--- src/com/yellerapp/client/ApacheHTTPClient.java
+++ src/com/yellerapp/client/ApacheHTTPClient.java
@@ -6,22 +6,37 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ApacheHTTPClient implements HTTPClient {
+    private static final int CONNECTION_MAX_LIMIT = 64;
+
 	private final HttpClient http;
 	private final ObjectMapper mapper;
 
 	public ApacheHTTPClient() {
-		this.http = new DefaultHttpClient();
+		this.http = buildHTTPClient();
 		http.getParams().setParameter("http.socket.timeout", new Integer(2000));
 		this.mapper = new ObjectMapper();
 		this.mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 	}
+
+    public HttpClient buildHTTPClient() {
+		RequestConfig requestConfig = RequestConfig.custom().
+				setSocketTimeout(2000).
+				setConnectTimeout(2000).build();
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(CONNECTION_MAX_LIMIT);
+		return HttpClients.custom().
+				setDefaultRequestConfig(requestConfig).
+				build();
+    };
 
 	public void post(String url, FormattedException exception)
 			throws IOException, AuthorizationException {
