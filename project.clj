c8c306f98f3c1e74dde8ec844b699feea59ea99b
Tom Crayford <tcrayford@googlemail.com>
2015-06-05 12:01:34 +0100

    bump jackson dependencies, version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yeller-java-api "0.4.1"
+(defproject yeller-java-api "1.1.0"
   :description "java client for yellerapp.com's exception tracking api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -8,9 +8,9 @@
   :javac-options ["-target" "1.7" "-source" "1.7"]
   :dependencies [[org.apache.httpcomponents/httpclient "4.3"]
                  [org.apache.httpcomponents/fluent-hc  "4.3"]
-                 [com.fasterxml.jackson.core/jackson-core "2.2.2"]
-                 [com.fasterxml.jackson.core/jackson-annotations "2.2.2"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.2.2"]]
+                 [com.fasterxml.jackson.core/jackson-core "2.3.2"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.3.2"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.3.2"]]
   :profiles {:test {:dependencies [[org.hamcrest/hamcrest-all "1.3"]
                                    [junit/junit-dep "4.11"]
                                    [org.jmock/jmock-junit4 "2.6.0"]
