aab8d24885e1bb79dd7db0418db66ebad39b5e74
Tom Crayford <tcrayford@googlemail.com>
2015-09-21 21:38:59 -0500

    bump version

--- src/com/yellerapp/client/Version.java
+++ src/com/yellerapp/client/Version.java
@@ -1,5 +1,5 @@
 package com.yellerapp.client;
 
 public class Version {
-    public final static String VERSION = "yeller-java: 1.3.1";
+    public final static String VERSION = "yeller-java: 1.4.2";
 }
